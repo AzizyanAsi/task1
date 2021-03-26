6812099caf4d3d465a0168bff360e6414ad34c65
AzizyanAsi <81428301+AzizyanAsi@users.noreply.github.com>
2021-03-26 08:11:13 -0700

    Add files via upload

---
+++ JavaLesson1/src/lesson1/Hello.java
@@ -0,0 +1,26 @@
+package lesson1;
+
+public class Hello {
+    public static void main(String[] args) {
+        byte a = 25;
+        System.out.println(a);
+        short c = -425;
+        System.out.println(c);
+        int number = 5;
+        System.out.println(number);
+        long y = 7485473544l;
+        System.out.println(y);
+        double m = 4.25;
+        System.out.println(m);
+        float f = 34.5f;
+        System.out.println(f);
+        boolean s = true;
+        System.out.println(s);
+        char k = '*';
+        System.out.println(k);
+        char money = '\u00a5';
+        System.out.println(money);
+        String text = "thank you";
+        System.out.println(text + " " + "Hayk");
+    }
+}
